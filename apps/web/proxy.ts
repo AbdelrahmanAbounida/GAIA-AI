1e0eef1ebf7639c133cd8163e4f56e52a634ae29
abdelrahman abounida <abdelrahmanaboneda@gmail.com>
2026-01-17 07:30:44 +0100

    refactor: use auth session in proxy

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getServerAuth } from "./lib/auth/actions";
+// import { getServerAuth } from "./lib/auth/actions";
 
 export async function proxy(request: NextRequest) {
-  //   return NextResponse.redirect(new URL("/home", request.url));
   const currentpath = request.nextUrl.pathname;
+  const user =
+    request.cookies.has("better-auth.session_token") ||
+    request.cookies.has("better_auth_session_token");
+  // const { user } = await getServerAuth();
 
-  // First Auth Layer
-  const { user } = await getServerAuth();
   if (!user && !(currentpath.startsWith("/auth") || currentpath === "/")) {
     return NextResponse.redirect(new URL("/auth", request.url));
   }
